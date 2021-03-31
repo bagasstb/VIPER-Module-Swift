de11b0c90c03b827aa37e6d2e01a4427e2dffd01
Satrio Teleng Bagaskoro <telengbagaskorosatrio@gmail.com>
2021-03-31 23:33:10 +0700

    Initial Commit

---
+++ Viper-Module-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Viper-Module-Swift
+//
+//  Created by bagasstb on 31/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Viper-Module-SwiftTests/Viper_Module_SwiftTests.swift
@@ -0,0 +1,33 @@
+//
+//  Viper_Module_SwiftTests.swift
+//  Viper-Module-SwiftTests
+//
+//  Created by bagasstb on 31/03/21.
+//
+
+import XCTest
+@testable import Viper_Module_Swift
+
+class Viper_Module_SwiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
